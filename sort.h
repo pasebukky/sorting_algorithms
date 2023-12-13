8a99a789ab5ea9f4587c448f517672b063fd2aa6
pasebukky <olubukunolapase@gmail.com>
2023-12-13 18:59:21 +0100

    Create README.md file and all essential startup files

---
+++ sort.h
@@ -0,0 +1,39 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+/* Function Prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+
+
+
+#endif
