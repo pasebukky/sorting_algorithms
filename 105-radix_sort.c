ef4c98c4120dac466937f10de497674302953232
0xNuru <nurudeenmyself@yahoo.com>
2023-12-18 16:13:52 +0100

    add radix_sort betty lintin

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -2,16 +2,17 @@
 
 /**
 * get_max - a function to get max element in array
-*
 * @array: the array to be sorted
 * @size: the size of the array
+*
+* Return: the max element
 */
 
 int get_max(int array[], size_t size)
 {
 	int max;
 	size_t i;
-	
+
 	/* Error Handling: array can't be empty */
 	if (size <= 0)
 		return (-1);
@@ -28,11 +29,11 @@ int get_max(int array[], size_t size)
 
 
 /**
-* main - entry point
-* @n: number..
+* count_sort - a function to sort an array using the count sort alogrithm
 *
-* Description: a function that prints the alphabet
-* Return: 0 (success)
+* @array: the array to be sorted
+* @size: the size of the array
+* @place_val: the plave value to be sorted
 */
 
 void count_sort(int array[], size_t size, int place_val)
@@ -51,11 +52,11 @@ void count_sort(int array[], size_t size, int place_val)
 	/* get the actueal postions from count */
 	for (i = 1; i < 10; i++)
 		count[i] = count[i] + count[i - 1];
-	
+
 	/* build new processed array L-R to maintain stability */
 	for (i = size - 1; (int)(i) >= 0; i--)
 		temp[--count[((array[i] / place_val) % 10)]] = array[i];
-	
+
 	/* populate initial array in sorted order */
 	for (i = 0; i < size; i++)
 		array[i] = temp[i];
@@ -77,9 +78,9 @@ void radix_sort(int *array, size_t size)
 	int max, place_val;
 
 	max = get_max(array, size);
-	
+
 	/* loop through all the place values in max */
-	for (place_val = 1; max/ place_val > 0; place_val *= 10)
+	for (place_val = 1; max / place_val > 0; place_val *= 10)
 	{
 		count_sort(array, size, place_val);
 		print_array(array, size);
